6bfe067b40ebebdaea3551d9a5cce3bab255bab8
loockeeer <lulu.r1313@gmail.com>
2025-06-03 15:00:53 +0200

    feat: finishes deserializer

--- serializer.c
+++ serializer.c
@@ -80,8 +80,34 @@ void metadata_free(void *data, metadata_provider_t metadata_provider) {
     if (metadata_provider == TWIXTLIVE) {
         free(data);
     }
+}
 
+moves_t *moves_create(move_type_t type, player_t player, int red_pos, int black_pos) {
+    moves_t *moves = malloc(sizeof(moves_t));
+    if (moves == NULL) return NULL;
+    moves->player = player;
+    if (moves->type == PEG) {
+        moves->peg.red_pos = red_pos;
+        moves->peg.black_pos = black_pos;
+    } else {
+        moves->peg.red_pos = -1;
+        moves->peg.black_pos = -1;
+    }
+    return moves;
+}
+moves_t *moves_append(moves_t *before, moves_t *new) {
+    if (before == NULL) return new;
+    if (new == NULL) return before;
+    new->next = before;
+    return new;
+}
 
+void moves_free(moves_t *moves) {
+    while (moves != NULL) {
+        moves_t *next = moves->next;
+        free(moves);
+        moves = next;
+    }
 }
 
 game_t *game_deserialize(char *buf) {
@@ -96,14 +122,98 @@ game_t *game_deserialize(char *buf) {
     }
     metadata_provider_t provider = metadata_deserialize(endptr, &(gt->metadata), &endptr);
     if (provider == -1) {
-        free(gt);
-        return NULL;
+        //free(gt);
+        //return NULL;
+        // je pense qu'on peut le garder comme ça
     }
     gt->provider = provider;
     cursor = endptr;
     cursor++;
-    // WIP
+    player_t player = -1;
+    int red_id = 0;
+    int black_id = 0;
+    int step = 0;
+    int substep = 1;
+    while (cursor != NULL && *cursor != '\0')
+    {
+        if (*cursor == ',') {
+            cursor++;
+            continue;
+        }
+        if (step == 0 && *cursor == 'R') {
+            player = RED;
+            step++;
+            cursor++;
+            continue;
+        }
+        if (step == 0 && *cursor == 'B') {
+            player = BLACK;
+            step++;
+            cursor++;
+            continue;
+        }
+
+        if (step == 1 && *cursor == '[') {
+            step++;
+            cursor++;
+            continue;
+        }
+
+        if (step == 1 && *cursor == 'R') {
+            gt->moves = moves_append(gt->moves, moves_create(RESIGN, player, -1, -1));
+            break;
+        }
+
+        if (step == 1 && *cursor == 'W') {
+            gt->moves = moves_append(gt->moves, moves_create(WIN, player, -1, -1));
+            break;
+        }
+        if (step == 2) {
+            if (*cursor >= 'A' && *cursor <= 'Z') {
+                // cursor is a caps letter
+                red_id += substep * (*cursor - 64);
+                substep*=26;
+                cursor++;
+                continue;
+            }
+            if (red_id == -1) {
+                game_free(gt);
+                return NULL;
+            }
+            if (*cursor >= '0' && *cursor <= '9') {
+                if (red_id < 0 || red_id > size) {
+                    free(gt);
+                    return NULL;
+                }
+                long int tmp = strtol(cursor, &endptr, 10);
+                if (tmp < 0 || tmp > size) {
+                    game_free(gt);
+                    return NULL;
+                }
+                black_id = (int)tmp;
+                step++;
+                substep = 1;
+                cursor = endptr;
+                continue;
+            }
+        }
+
+        if (step == 3 && *cursor == ']') {
+            step = 0;
+            cursor++;
+            gt->moves = moves_append(gt->moves, moves_create(PEG, player, red_id, black_id));
+            continue;
+        }
+        game_free(gt);
+        return NULL;
+    }
     return gt;
 }
-char *game_serialize(game_t *game);
-void game_free(game_t *game);
+char *game_serialize(game_t *game) {
+    return "WIP";
+}
+void game_free(game_t *game) {
+    moves_free(game->moves);
+    if (game->board != NULL) twixt_destroy(game->board);
+    free(game);
+}
--- serializer.h
+++ serializer.h
@@ -17,20 +17,33 @@ typedef struct metadata_twixtlive_s {
     long int blackid;
 } metadata_twixtlive_t;
 
-metadata_twixtlive_t *metadata_twixtlive_deserialize(char *buf);
+metadata_twixtlive_t *metadata_twixtlive_deserialize(char *buf, char **end);
 char *metadata_twixtlive_serialize(metadata_twixtlive_t *twixtlive);
 
-metadata_provider_t metadata_deserialize(char *buf, void **dst);
+metadata_provider_t metadata_deserialize(char *buf, void **dst, char **end);
 char *metadata_serialize(void *data, metadata_provider_t metadata_provider);
 void metadata_free(void *data, metadata_provider_t metadata_provider);
 
+typedef enum move_type {
+    PEG,
+    SWAP,
+    RESIGN,
+    WIN
+} move_type_t;
+
 typedef struct moves_s {
+    move_type_t type;
     player_t player;
-    int red_pos;
-    int black_pos;
-    bool swap;
-    bool win;
-} moves_t; // if swap is true -> player swapped. if win is true -> player won. otherwise player paced a peg. ALL! possible links created
+    struct {
+        int red_pos;
+        int black_pos;
+    } peg;
+    struct moves_s *next;
+} moves_t;
+
+moves_t *moves_create(move_type_t type, player_t player, int red_pos, int black_pos);
+moves_t *moves_append(moves_t *before, moves_t *new);
+void moves_free(moves_t *moves);
 
 typedef struct game_s {
     metadata_provider_t provider;
