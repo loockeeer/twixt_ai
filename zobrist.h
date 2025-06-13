10a6cc47c42296ef2ec9bfe7a51eb3525049e105
loockeeer <lulu.r1313@gmail.com>
2025-06-13 14:35:29 +0200

    feat: add player field to zobrist header

--- zobrist.h
+++ zobrist.h
@@ -11,7 +11,7 @@ typedef struct zobrist_s zobrist_t;
 
 zobrist_t *zobrist_create(int zoom_count, int *zooms);
 void zobrist_destroy(zobrist_t *zobrist);
-float zobrist_evaluate(zobrist_t *zobrist, position_t position, board_t *board);
+float zobrist_evaluate(zobrist_t *zobrist, position_t position, player_t player, board_t *board);
 
 void zobrist_update(zobrist_t *zobrist, game_t *game);
 char *zobrist_serialize(zobrist_t *zobrist);
